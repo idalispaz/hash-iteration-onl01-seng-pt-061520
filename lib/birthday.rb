847b68fc4d0de95f01888bd2ed63ec38d573f3b0
Idalis Paz <idalispaz@gmail.com>
2020-06-27 22:18:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,7 @@
 def happy_birthday(birthday_kids)
   happy_birthday(birthday_kids)
     birthday_kids.each do |kids_name, age|
-      puts "Hapy Birthday #{kids_name}! You are now #{age} years old!"
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
 end
 
