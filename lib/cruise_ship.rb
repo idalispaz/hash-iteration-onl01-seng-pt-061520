d3c71f675b5c91b55a54ea1f8e9b5a15c690e468
Idalis Paz <idalispaz@gmail.com>
2020-06-27 22:10:49 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,9 +7,9 @@
 # }
 
 def select_winner(passengers)
-  winner = ""
+  winner = ("")
   passengers.each do |suite, name|
-    if suite == :suite_A  &&
+    if suite == :suite_a &&
       name.start_with? ( "A" )
       winner = name 
     end
